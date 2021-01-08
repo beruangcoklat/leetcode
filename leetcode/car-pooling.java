b42e231cfbc06ae4f9e811394778a5d55d1751ef
yulian <yuliangunawan19@gmail.com>
2021-01-08 13:12:45 +0700

    Create car-pooling.java

---
+++ leetcode/car-pooling.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        HashMap<Integer, Integer> memo = new HashMap();
+        for(int i=0 ; i<trips.length ; i++){
+            int numsPassengers = trips[i][0];
+            int startLocation = trips[i][1];
+            int endLocation = trips[i][2];
+            
+            for(int j=startLocation ; j< endLocation ; j++){
+                int total = numsPassengers;
+                if(memo.containsKey(j)){
+                    total += memo.get(j);
+                }
+                if(total > capacity){
+                    return false;
+                }
+                memo.put(j, total);
+            }
+        }
+        return true;
+    }
+}
