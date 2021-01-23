b038d50fd07634054477d966b5b57c23c7c93465
beruangcoklat <yuliangunawan19@gmail.com>
2021-01-23 19:19:37 +0700

    Best Time to Buy and Sell Stock III

---
+++ leetcode/Best Time to Buy and Sell Stock III/1.java
@@ -0,0 +1,36 @@
+class Solution {
+    HashMap<String, Integer> memo = new HashMap<>();
+
+    int solve(int idx, int k, int[] prices, int state, int tx) {
+        if (idx == prices.length || tx == k) {
+            return 0;
+        }
+
+        String key = state + "-" + idx + "-" + tx;
+        Integer mem = memo.get(key);
+        if (mem != null) {
+            return mem;
+        }
+
+        int wait = solve(idx + 1, k, prices, state, tx);
+        int max = wait;
+
+        if (state == 0) {
+            int buy = solve(idx + 1, k, prices, state + 1, tx);
+            buy -= prices[idx];
+            max = Math.max(buy, wait);
+        } else if (state == 1) {
+            int sell = solve(idx + 1, k, prices, state - 1, tx + 1);
+            sell += prices[idx];
+            max = Math.max(sell, wait);
+        }
+
+        memo.put(key, max);
+        return max;
+    }
+
+    public int maxProfit(int[] prices) {
+        memo = new HashMap<>();
+        return solve(0, 2, prices, 0, 0);
+    }
+}
