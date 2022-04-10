49e66f029ccc1d797267e3791aab6ba84b4859b7
beruangcoklat <yuliangunawan19@gmail.com>
2022-04-10 08:46:43 +0700

    Contains Duplicate

---
+++ leetcode/Contains Duplicate/1.go
@@ -0,0 +1,10 @@
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]struct{})
+	for _, n := range nums {
+		if _, ok := m[n]; ok {
+			return true
+		}
+		m[n] = struct{}{}
+	}
+	return false
+}
