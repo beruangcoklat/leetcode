3e601639e6c914f303e2b503aa2da8e22ab8b878
beruangcoklat <yuliangunawan19@gmail.com>
2022-04-15 11:07:47 +0700

    Trim a Binary Search Tree

---
+++ leetcode/Trim a Binary Search Tree/2.go
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findReplacer(root *TreeNode, low, high int) *TreeNode {
+	for root != nil && (root.Val > high || root.Val < low) {
+		if root.Val > high {
+			root = root.Left
+		} else {
+			root = root.Right
+		}
+	}
+	return root
+}
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val > high || root.Val < low {
+		root = findReplacer(root, low, high)
+	}
+
+	queue := []*TreeNode{root}
+
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		if curr == nil {
+			continue
+		}
+
+		if curr.Right != nil && curr.Right.Val > high {
+			curr.Right = findReplacer(curr.Right, low, high)
+		}
+
+		if curr.Left != nil && curr.Left.Val < low {
+			curr.Left = findReplacer(curr.Left, low, high)
+		}
+
+		queue = append(queue, curr.Right, curr.Left)
+	}
+
+	return root
+}
