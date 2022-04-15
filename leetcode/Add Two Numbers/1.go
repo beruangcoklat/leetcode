009165acab60dca351450a5d0c6bb9265c28dd5c
yulian <yulian@bareksa.com>
2022-04-15 17:02:52 +0700

    refactor Add Two Numbers golang

--- leetcode/Add Two Numbers/1.go
+++ leetcode/Add Two Numbers/1.go
@@ -1,5 +1,3 @@
-// https://leetcode.com/problems/add-two-numbers/
-
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -7,64 +5,42 @@
  *     Next *ListNode
  * }
  */
+func push(head, tail *ListNode, val int) (*ListNode, *ListNode) {
+	if head == nil {
+		head = &ListNode{Val: val}
+		tail = head
+	} else {
+		tail.Next = &ListNode{Val: val}
+		tail = tail.Next
+	}
+	return head, tail
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var curr, head *ListNode
-	var send int = 0
-	for l1 != nil && l2 != nil {
-		total := l1.Val + l2.Val
-		if send == 1 {
-			total += 1
-			send = 0
+	var head, tail *ListNode
+	carrier := 0
+	for l1 != nil || l2 != nil {
+		a, b := 0, 0
+		if l1 != nil {
+			a = l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			b = l2.Val
+			l2 = l2.Next
 		}
 
+		total := a + b + carrier
+		carrier = 0
 		if total >= 10 {
-			send = 1
 			total -= 10
+			carrier = 1
 		}
-		newnode := &ListNode{
-			Val:  total,
-			Next: nil,
-		}
-
-		if head == nil {
-			head = newnode
-			curr = head
-		} else {
-			curr.Next = newnode
-			curr = newnode
-		}
-
-		l2 = l2.Next
-		l1 = l1.Next
+		head, tail = push(head, tail, total)
 	}
 
-	var l3 *ListNode
-	if l1 != nil {
-		l3 = l1
-	}
-	if l2 != nil {
-		l3 = l2
-	}
-
-	for l3 != nil {
-		if send == 1 {
-			l3.Val += 1
-			send = 0
-		}
-		if l3.Val >= 10 {
-			l3.Val -= 10
-			send = 1
-		}
-		curr.Next = l3
-		curr = l3
-		l3 = l3.Next
-	}
-
-	if send == 1 {
-		curr.Next = &ListNode{
-			Next: nil,
-			Val:  1,
-		}
+	if carrier > 0 {
+		head, tail = push(head, tail, carrier)
 	}
 	return head
 }
